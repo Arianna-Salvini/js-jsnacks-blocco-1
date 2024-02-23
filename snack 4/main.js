cc97dbaf0186fdfc8985133b6f2c5e6991c6d0f5
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-23 14:28:29 +0100

    Fix snack 4

--- snack 4/main.js
+++ snack 4/main.js
@@ -1,23 +1,25 @@
 console.log("Hello, from Gatsby");
 
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby
-let userName = prompt("What's your name?");
-const invitedFromGatsby = [ "Nick Carraway", "Daisy Buchanan", "Tom Buchanan", "Jordan Baker", "Arianna  Salvini"]
-console.log(invitedFromGatsby);
 // chiedi all’utente il suo nome 
-let invited = false;
+const invitedFromGatsby = [ "Nick Carraway", "Daisy Buchanan", "Tom Buchanan", "Jordan Baker", "Arianna Salvini"]
+let userName = prompt("What's your name?");
+let invitedToParty = false ;
 
 //  comunicagli se può partecipare o no alla festa
-for (let i = 0; i < invitedFromGatsby.length; i++){
-    let userCheck = invitedFromGatsby[i]
 
-    if (userName = userCheck ){
-        invited = true
-    } 
+for (let i = 0; i < invitedFromGatsby.length; i++) {
+    let inviteCheck = invitedFromGatsby[i];
+    
+    if (userName == inviteCheck) {
+        invitedToParty = true
+        console.log(invitedToParty);
+    }   
 }
 
-if (invited){
-    console.log("Hello from Gatsby")
+if (invitedToParty) {
+    console.log("Gatsby, is happy to see you!");
 } else {
-    console.log("Sorry, ypur name is not on the list");
+    console.log("Sorry, your name is not in the list, you're not invited.");    
 }
+    
