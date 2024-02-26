ddc8054f619e611acdf13f31b4f2865f51dd87b2
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-26 13:22:41 +0100

    Fix code

--- snack 5/main.js
+++ snack 5/main.js
@@ -3,10 +3,11 @@ console.log('hello');
 const listOddNumber = []
 
 for (let i = 0; i < 6; i++) {
-    let inputNumber = Number(prompt('Inserisci un numero'))
+    let inputNumber = parseInt(prompt('Inserisci un numero'))
     
-inputNumber % 2 !== 0 ? inputNumber.push(listOddNumber)
-
+if (inputNumber % 2 !== 0) {
+    listOddNumber.push(inputNumber)
+}
 }
 
 
