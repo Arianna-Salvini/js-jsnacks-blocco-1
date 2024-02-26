9ac3ca0c9d2cab205e657413977e90eedeb982a3
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-26 15:50:45 +0100

    Init

---
+++ snack afternoon 1/main.js
@@ -0,0 +1,12 @@
+console.log('Hi there!');
+
+// Genera un numero casuale compreso tra 1 e 100. 
+let numberRandom  = Math.round(Math.random() * 100) +1;
+console.log(numberRandom);
+
+// Chiedi all'utente di indovinare il numero. 
+const message = prompt("Indovina il numero da 1 a 100")
+
+// Dopo ogni tentativo, fornisci un suggerimento se il numero inserito è maggiore o minore del numero segreto. 
+
+// Fornisci anche il numero totale di tentativi effettuati.
