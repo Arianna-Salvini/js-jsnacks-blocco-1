65fed4e610d57c3c23dac2deb4212bb7a583776d
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-26 12:11:11 +0100

    Init

---
+++ snack 5/main.js
@@ -0,0 +1,13 @@
+console.log('hello');
+
+const listOddNumber = []
+
+for (let i = 0; i < 6; i++) {
+    let inputNumber = Number(prompt('Inserisci un numero'))
+    
+inputNumber % 2 !== 0 ? inputNumber.push(listOddNumber)
+
+}
+
+
+console.log(listOddNumber);
