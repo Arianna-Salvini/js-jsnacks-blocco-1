a8b2492e209959684aefae258d2dfea2ab76ea40
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-26 14:38:52 +0100

    Fix code finally

--- snack 8/main.js
+++ snack 8/main.js
@@ -3,14 +3,14 @@ console.log('hello');
 // Chiedi un numero di 4 cifre all’utente
 // Calcola la somma di tutte le cifre che compongono il numero.
 
-let numb4Digit = Number(prompt("inserisci un numero a 4 cifre"));
-console.log(numb4Digit);
+let numb4Digit = (prompt("inserisci un numero a 4 cifre"));
+// console.log(numb4Digit);
 let sum = 0
 
-if ( numb4Digit.length === 4){
+for (let i = 0; i < numb4Digit.length; i++) {
+    let digitN = parseInt(numb4Digit[i])
+    sum += digitN
 
-    for (let i = 0; i < numb4Digit.length; i++) {
-        sum += digit[i]
-        console.log(sum);
-    }
+    console.log(sum);
 }
+
