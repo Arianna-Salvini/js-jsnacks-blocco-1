939d8b571c0dab2789c64fc25fb1f0157f08b769
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-26 13:03:28 +0100

    start for cile

--- snack 8/main.js
+++ snack 8/main.js
@@ -1,1 +1,14 @@
 console.log('hello');
+
+// Chiedi un numero di 4 cifre all’utente
+// Calcola la somma di tutte le cifre che compongono il numero.
+
+let numb4Digit = Number(prompt("inserisci un numero a 4 cifre"));
+console.log(numb4Digit);
+let sum = 0
+
+for (let i = 0; i < numb4Digit.length; i++) {
+    let digit = numb4Digit[i];
+    console.log(digit);
+    
+}
