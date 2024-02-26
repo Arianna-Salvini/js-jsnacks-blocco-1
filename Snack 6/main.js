3695ee10212c59fde3f8ee1a4d4cd7b08b1702e2
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-26 13:28:35 +0100

    fix code

--- Snack 6/main.js
+++ Snack 6/main.js
@@ -1,9 +1,10 @@
 console.log('hello');
 
-let n = prompt("Inserisci il numero da levar al cubo")
+let n = parseInt(prompt("Inserisci il numero da elvare al cubo"))
 // let cube = n * n
 
-for (let i = 0; i < n ; i++) {
-    
+for (let i = 0; i < n +1 ; i++) {
+    let cube = i ** 3
+    console.log(cube);
     
 }
