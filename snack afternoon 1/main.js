6592f39cb358a16debb2c8556e633c2905c27004
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-26 16:23:45 +0100

    Add do while

--- snack afternoon 1/main.js
+++ snack afternoon 1/main.js
@@ -5,8 +5,27 @@ let numberRandom  = Math.round(Math.random() * 100) +1;
 console.log(numberRandom);
 
 // Chiedi all'utente di indovinare il numero. 
-const message = prompt("Indovina il numero da 1 a 100")
 
 // Dopo ogni tentativo, fornisci un suggerimento se il numero inserito è maggiore o minore del numero segreto. 
+let checkedNumber = true;
+let tryNumb = 0
+
+do{
+    const userNumber = prompt("Indovina il numero da 1 a 100")
+   
+    if (userNumber < numberRandom ) {
+        alert( `Il numero è più grande di ${userNumber}`);
+    } else if (userNumber > numberRandom){
+        alert(`Il numero è minore di ${userNumber} `);
+    } else {
+       checkedNumber = false;
+    }
+
+ tryNumb++
+
+} while (checkedNumber=false ) {
+    alert(`Hai vinto! Dopo ${tryNumb} tentativi`)
+}
+
 
 // Fornisci anche il numero totale di tentativi effettuati.
